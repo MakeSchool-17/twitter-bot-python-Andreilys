89fb3e4fa2a2b3913d4d8ad14aec9ee4e346798b
Andrei Lyskov <andreilyskov@gmail.com>
2015-09-25 10:42:11 -0700

    adding dictionary_Words.py

---
+++ dictionary_words.py
@@ -0,0 +1,22 @@
+file = open('stoicism.txt')
+
+a_string = file.read()
+
+# print("%s" % a_string)
+#
+# file1 = file.read(16)
+#
+# print("%s" % file1)
+
+
+# make a function that opens the file/reads file
+
+
+# return max # of words in file+use as a ceiling for random generator
+
+
+# loop argv number of times and add to a new list
+
+# Main Function
+if __name__ == '__main__':
+    print("Hello")
