042c3f7f6d28fff7866cb569607a23995a70a3bf
Andrei Lyskov <andreilyskov@gmail.com>
2015-10-10 18:00:58 -0700

    Did some more work on markov chain

--- markovChain.py
+++ markovChain.py
@@ -1,9 +1,9 @@
 import text_tokenizer
 from text_tokenizer import *
+import random
 
 
 class SentenceGenerator(object):
-
     def __init__(self, size):
         self.size = size
 
@@ -11,25 +11,27 @@ class SentenceGenerator(object):
         tokenized_text.remove('')
         length = len(tokenized_text) - 1
         i = 0
-        dictionary = [dict() * self.size]
+        List = []
         for word in tokenized_text:
             if i != length:
+                dictionary = dict()
                 dictionary[word] = tokenized_text[i + 1]
+                List.append(dictionary)
                 i = i + 1
-        return dictionary
+        return List
 
-
-    def generate_sentences(self, dictionary):
+    def generate_sentences(self, List):
         sentence = ""
-        for key in dictionary:
-            sentence = sentence + ' ' + key
-            sentence = sentence + ' ' + dictionary[key]
-        print(sentence)
+        length = len(List)
+        for i in range(5):
+            random_Number = random.randrange(0, length, 1)
+            sentence = sentence + " " + List[random_Number].values()[0] + " " + List[random_Number].keys()[0]
+        return sentence
 
 if __name__ == '__main__':
     # tokenize the text
     words = "text.txt"
     tokenized_text = text_Parser(words)
     sentence_generator = SentenceGenerator(10)
-    dictionary = SentenceGenerator.build_dict(tokenized_text)
-    print(sentenceGenerator.generate_sentences(dictionary))
+    List = sentence_generator.build_dict(tokenized_text)
+    print(sentence_generator.generate_sentences(List))
